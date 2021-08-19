0803036bdc114dcc184737631bd999bd238a0710
SunnyNie <sunnymingnie@gmail.com>
2021-08-18 21:23:05 -0700

    add strats for portfolio

--- model/printer.py
+++ model/printer.py
@@ -2,9 +2,10 @@ from strategy import Strategy
 from model import Model
 
 strats = [
-    Strategy("ARDRUSDT", "LSKUSDT", 4, 0.2, 6100, 0.7),
-    Strategy("CELRUSDT", "FETUSDT", 3, 0.2, 4000, 0.4),
-    Strategy("", "", 3, 0, 4000, 0.5)
+    Strategy("ARDRUSDT", "LSKUSDT", 4, -0.1, 6100, 0.7),
+    Strategy("NKNUSDT", "WINUSDT", 4.3, -0.6, 5500, 0.5),
+    Strategy("CELRUSDT", "ONGUSDT", 4.3, -0.2, 4200, 0.4), 
+    Strategy("CHZUSDT", "ENJUSDT", 4.3, -0.5, 4500, 0.4)
 ]
 printer = Model(strats)
 printer.turn_on()
